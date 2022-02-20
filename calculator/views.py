632aa1ea7fa305eb9f4193b9abdaf15dbb8f5e97
Pavel Beloglazov <beloglazov_pl@icloud.com>
2022-02-20 23:06:00 +1000

    Make universal view function with recipes

--- calculator/views.py
+++ calculator/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render
+
 
 DATA = {
     'omlet': {
@@ -19,62 +20,17 @@ DATA = {
 }
 
 
-def home(request):
-    pages = {
-        'Омлет': reverse('omlet'),
-        'Паста': reverse('pasta'),
-        'Бутерброд': reverse('buter')
-    }
-    context = {
-        'pages': pages
-    }
-    return render(request, 'calculator/home.html', context)
-
-
-def omlet(request):
+def dish(request, dish):
     person = int(request.GET.get('servings', 1))
+    recipe_new = {}
+    if dish in DATA.keys():
+        for ingredient, amount in DATA[dish].items():
+            amount_new = amount * person
+            recipe_new[ingredient] = amount_new
+    else:
+        dish = ''
     context = {
-        'dish': 'Омлет',
-        'recipe': {
-            'яйца, шт': 2 * person,
-            'молоко, л': round(0.1 * person, 1),
-            'соль, ч.л.': round(0.5 * person, 1),
-        }
+        'dish': dish.capitalize(),
+        'recipe': recipe_new
     }
     return render(request, 'calculator/index.html', context)
-
-
-def pasta(request):
-    person = int(request.GET.get('servings', 1))
-    context = {
-        'dish': 'Паста',
-        'recipe': {
-            'макароны, г': round(0.3 * person, 1),
-            'сыр, г': round(0.05 * person, 2),
-        }
-    }
-    return render(request, 'calculator/index.html', context)
-
-
-def buter(request):
-    person = int(request.GET.get('servings', 1))
-    context = {
-        'dish': 'Бутерброд',
-        'recipe': {
-            'хлеб, ломтик': 1 * person,
-            'колбаса, ломтик': 1 * person,
-            'сыр, ломтик': 1 * person,
-            'помидор, ломтик': 1 * person,
-        }
-    }
-    return render(request, 'calculator/index.html', context)
-
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
--- recipes/urls.py
+++ recipes/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 
 from django.urls import path
-from calculator.views import home, omlet, pasta, buter
+from calculator.views import dish
+
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('omlet/', omlet, name='omlet'),
-    path('pasta/', pasta, name='pasta'),
-    path('buter/', buter, name='buter')
+    path('<str:dish>/', dish)
 ]
